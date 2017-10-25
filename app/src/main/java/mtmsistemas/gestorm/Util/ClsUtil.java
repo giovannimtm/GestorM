35bd4269de39c8378a16f7efcf181598eb0d61bd
JeanZanellaMTM <jean@mtmsistemas.com.br>
2017-10-25 09:58:14 -0200

    Criadas as funções de validação para verificar se a data informada é anterior a atual, validar o formato da placa e a função para ler a imagem de um caminho e transformar em um array de bytes para poder salvar no banco de dados

--- app/src/main/java/mtmsistemas/gestorm/Util/ClsUtil.java
+++ app/src/main/java/mtmsistemas/gestorm/Util/ClsUtil.java
@@ -1,8 +1,67 @@
 package mtmsistemas.gestorm.Util;
 
+import android.text.format.DateUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Calendar;
+import java.util.regex.Pattern;
+
 /**
  * Created by Giovanni on 19/10/2017.
  */
 
 public class ClsUtil {
+
+    public static boolean FU_dataAnteriorAHoje(String data) {
+        Boolean validou;
+        try {
+            Calendar now = Calendar.getInstance();
+            now.set(Calendar.HOUR, 0);
+            now.set(Calendar.HOUR_OF_DAY, 0);
+            now.set(Calendar.MINUTE, 0);
+            now.set(Calendar.SECOND, 0);
+            now.set(Calendar.MILLISECOND, 0);
+
+            SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+            Date dataFormatada = formatador.parse(data);
+
+            Date hoje = now.getTime();
+            validou = dataFormatada.before(hoje);
+        }
+        catch(ParseException ex){
+            ex.printStackTrace();
+            validou = false;
+        }
+        return validou;
+    }
+
+    public static boolean FU_validaPlaca(String placa){
+        return Pattern.matches("[A-Z]{3}-[0-9]{4}",placa);
+    }
+
+    public byte[] FU_leImagem(String file) {
+        ByteArrayOutputStream bos = null;
+        try {
+            File f = new File(file);
+            FileInputStream fis = new FileInputStream(f);
+            byte[] buffer = new byte[1024];
+            bos = new ByteArrayOutputStream();
+            for (int len; (len = fis.read(buffer)) != -1;) {
+                bos.write(buffer, 0, len);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e2) {
+            System.err.println(e2.getMessage());
+        }
+        return bos != null ? bos.toByteArray() : null;
+    }
+
 }
